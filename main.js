477fe484d907522593682650445d6f4240c9b10f
Gport-Gr <mclaren197@yahoo.gr>
2022-10-03 20:08:48 +0300

    bug1

--- main.js
+++ main.js
@@ -28,15 +28,12 @@ const hamburger = document.getElementById('menu-btn');
 const navUL = document.getElementById('list');
 const menu = document.getElementById('myHeader');
 const photo = document.getElementById('mainphoto');
-const photob = document.getElementById('mainphotob');
 const head = document.getElementById('head');
 
     hamburger.addEventListener('click', () => {
         navUL.classList.toggle('show');
         menu.classList.toggle('show');
         photo.classList.toggle('show');
-        photobio.classList.toggle('show');
-        
         head.classList.toggle('show');
     });
 
